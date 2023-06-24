b97bf7324307f2c4ff5f68255d309404c3890e2a
LeleStacia <km8003296@gmail.com>
2023-06-25 00:40:19 +0700

    Setup Data Module

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -25,10 +26,26 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+}
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:remote"))
+
+    //  Coroutine
+    implementation(libs.coroutine)
+    testImplementation(libs.coroutine.test)
+
+    //  Hilt
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.espresso.core)
 }
